6b82951ee21de968dd89685c0b82d8c728cb4b2d
supun <supun.ishara@eimsky.com>
2022-08-03 15:38:44 +0530

    dowonlod view

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import logo from "./logo.svg";
 import "./App.css";
 import "antd/dist/antd.css";
 import { Col, Row, Image } from "antd";
-import { useState,useEffect } from "react";
+import { useState, useEffect } from "react";
 import NavBarWeb from "./Components/Navbar/NavBarWeb";
 import FooterWeb from "./Components/Footer/FooterWeb";
 import MapView from "./Components/Map/MapView";
@@ -17,16 +17,23 @@ import right_arrow from '../src/assets/right_arrow.svg';
 import search_icon from '../src/assets/search icon.svg';
 import app_store from '../src/assets/app_store.svg';
 import google_play from '../src/assets/google-play1.svg';
+import downlod_home from '../src/assets/downlod_home.svg';
+import downlod_tag from '../src/assets/downlod-tag.svg';
+import downlod_plnner from '../src/assets/downlod-plnner.svg';
+import downlod_close from '../src/assets/downlod-close.svg';
+import downlod from '../src/assets/downlod.svg';
 
 import axios from 'axios';
 
 
 function App() {
+
   const [apiArray, setapiArray] = useState([]);
+  const [isShown, setIsShown] = useState(true);
   useEffect(() => {
-    
+
     (async () => {
-      
+
       try {
         const response = await axios.get('http://ec2-13-211-131-132.ap-southeast-2.compute.amazonaws.com:3000/api/search?lat=-35.3810867&lng=174.0588784&radius=1000&showCoupons=false&categories=adventure^financial_services^relax&id=sudesh_android_1235');
         setapiArray(response.data.data);
@@ -34,15 +41,22 @@ function App() {
       } catch (error) {
         console.log(error);
       } finally {
-       
+
       }
     })();
   }, []);
- 
+  const handleDwonlodCloseClick = (e) => {
+    setIsShown(e);
+  }
+  const handleDwonlodClick = (e) => {
+    console.log(e);
+    setIsShown(e);
+  };
+
   return (
     <div className="App" >
       <>
-        <NavBarWeb />
+        <NavBarWeb parentCallback={handleDwonlodClick} />
         <link
           href="http://fonts.cdnfonts.com/css/circular-std"
           rel="stylesheet"
@@ -76,11 +90,11 @@ function App() {
               </Row>
             </div>
             <Col xs={24} xl={15} span={15} className="map-view-col">
-              {apiArray.length>0?(<MapView details={{apiArray}}/>):('')}
-              
+              {apiArray.length > 0 ? (<MapView details={{ apiArray }} />) : ('')}
+
             </Col>
             <Col xs={24} xl={1} span={15}></Col>
-            <Col xs={24} xl={8} span={9} className="web-only web-only-2">
+            <Col xs={24} xl={8} span={9} className="web-only web-only-2" style={{ display: isShown ? 'block' : 'none' }}>
               <Row className="side-row web-only">
                 <Col xs={24}>
                   {/* <InputGroup>
@@ -168,6 +182,143 @@ function App() {
                     <img src={right_arrow} alt="Logo" />
                   </a>
                 </Col>
+              </Row>
+            </Col>
+            <Col xs={24} xl={8} span={9} className="web-only web-only-2" style={{ display: isShown ? 'none' : 'block' }}>
+
+              <div style={{ zIndex: 1000 }} class="fab2">
+                <a onClick={(e) => {
+                  handleDwonlodCloseClick(true);
+                }} style={{ padding: '0px 4px 11px 6px' }} >
+                  <img src={downlod_close} alt="Logo" />
+                </a>
+
+
+
+
+              </div>
+              <Row className="side-row web-only">
+                <Col xs={24} className="first-image-view">
+                  <div className="first-image-view">
+                    <Image src="../images/downlod.svg" />
+                  </div>
+                </Col>
+
+                <Col xs={24}>
+                  <div className="first-image-view2">
+                    <h4
+                      style={{ textAlign: "center", fontSize: "20px" }} // className="txt1"
+                    >
+                      Download the app to add places to save places you like
+                    </h4>
+
+
+                  </div>
+                </Col>
+              </Row>
+
+              <Row className="fist-row-set">
+
+                <Col xs={8} >
+                  <a
+                    className="find-awesome-places-icon"
+                    href="#"
+                    style={{ padding: "18px 4px 11px 5px" }}
+                  >
+                    <img src={downlod_home} alt="Logo" />
+                  </a>
+                </Col>
+                <Col xs={8} style={{ display: 'flex', justifyContent: 'center' }}>
+
+                  <a
+                    className="add-to-planner-icon"
+                    href="#"
+                    style={{ padding: "5px 5px 5px 5px", position: 'absolute', bottom: 0 }}
+                  >
+                    <img src={downlod_plnner} alt="Logo" />
+                  </a>
+                </Col>
+                <Col xs={8} >
+                  <a
+                    className="save-with-coupons-icon"
+                    href="#"
+                    style={{ padding: "18px 4px 11px 5px" }}
+                  >
+                    <img src={downlod_tag} alt="Logo" />
+                  </a>
+                </Col>
+              </Row>
+              <Row className="second-row-set">
+
+                <Col xs={8} >
+                  <a
+                    className="find-awesome-places-icon2"
+                    href="#"
+                    style={{ padding: " 4px 9px 4px 5px" }}
+                  >
+
+                  </a>
+                </Col>
+                <Col xs={8} style={{ display: 'flex', justifyContent: 'center' }}>
+
+                  <a
+                    className="add-to-planner-icon2"
+                    href="#"
+                    style={{ padding: "4px 1px 4px 5px", position: 'absolute', bottom: 0 }}
+                  >
+
+                  </a>
+                </Col>
+                <Col xs={8} >
+                  <a
+                    className="save-with-coupons-icon2"
+                    href="#"
+                    style={{ padding: "4px 1px 4px 5px" }}
+                  >
+
+                  </a>
+                </Col>
+              </Row>
+              <Row style={{ paddingTop: 17 }} >
+
+                <Col xs={8} className="downlod-font-row" >
+
+
+                  Find awesome places
+
+                </Col>
+                <Col xs={8} className="downlod-font-row" >
+
+                  Add to Planner
+
+                </Col>
+                <Col xs={8} className="downlod-font-row"  >
+                  Save with coupons
+                </Col>
+              </Row>
+              <Row>
+                <div class="icons" style={{ paddingBlock: 40 }}>
+                  <a className="downlod-icon-1" href="#">
+                    <img style={{ width: "18%" }} src={google_play} alt="Logo" />{" "}
+                    <span style={{ fontFamily: "Outfit", fontWeight: 700 }}>
+                      Google Play
+                    </span>
+                  </a>
+                  <a className="downlod-icon-1" href="#">
+                    <img
+                      style={{
+                        width: "18%",
+                        fontFamily: "Outfit",
+                        fontWeight: 700,
+                      }}
+                      src={app_store}
+                      alt="Logo"
+                    />{" "}
+                    <span style={{ fontFamily: "Outfit", fontWeight: 700 }}>
+                      App Store
+                    </span>
+                  </a>
+                </div>
               </Row>
             </Col>
           </Row>
